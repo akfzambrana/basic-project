5a0cac946715f34c0de3030947df15ad0989651f
Andrea Zambrana <azambrana@prestadores.amil.com.br>
2016-09-06 17:16:48 -0300

    Installing and requiring browsersync task and dependency

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,6 @@
 require('./tasks/clean.js');
 require('./tasks/styles.js');
 require('./tasks/copy.js');
+require('./tasks/browsersync.js');
 require('./tasks/watch.js');
 require('./tasks/default.js');
