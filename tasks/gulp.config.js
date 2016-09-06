2b2f5fc81d1b4f7072bc162af2d4582f76c9f74f
Andrea Zambrana <azambrana@prestadores.amil.com.br>
2016-09-06 15:56:53 -0300

    Adding some examples in the gulp.config file

--- tasks/gulp.config.js
+++ tasks/gulp.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   sourceDir: './assets',
   buildDir: './dist',
-  htmlDestDir: '/',
-  resourcesDestDir: '/',
+  htmlDestDir: '/', // ex: '/html'
+  resourcesDestDir: '/', // ex: '/public'
   browsersSupport: [
     'last 2 versions',
     '> 5%',
